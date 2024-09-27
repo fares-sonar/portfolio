43519a9f1059c7097865b90669455e35d925b2dd
fares sonar <95088203+fares-sonar@users.noreply.github.com>
2024-09-27 18:14:45 +0300

    Update Footer.jsx

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <footer className="flex flex-wrap md:justify-between justify-center md:spacey-0 space-y-2 my-12 ">
+    <footer className="flex flex-wrap md:justify-between justify-center md:spacey-0 space-y-2 my-12 pt-9 ">
       <ul className="flex flex-wrap gap-4">
         <li><a className="text-title text-[.88rem] opacity-90 font-medium hover:text-[.9rem] hover:text-blue hover:opacity-100 transition-colors  " href="#">Home</a></li>
         <li><a className="text-title text-[.88rem] opacity-90 font-medium hover:text-[.9rem] hover:text-blue hover:opacity-100 transition-colors  " href="#">Articles</a></li>
